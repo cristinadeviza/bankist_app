6d821d515af69c7d5b6748f85147b6d61bd456c6
Deviza Cristina <deviza.cristina.98@gmail.com>
2024-12-09 13:28:40 +0100

    second branch

--- script.js
+++ script.js
@@ -87,7 +87,16 @@ const displayMovements = function(movements){
     
 
 displayMovements(account1.movements);
-console.log(containerMovements.innerHTML);
+
+
+const createUsernames = function(accs){
+  accs.forEach(function(acc){
+    acc.username= acc.owner.toLowerCase().split(' ').map(name => name[0]).join('')
+
+  });
+};
+createUsernames(accounts);
+
 
 
 
